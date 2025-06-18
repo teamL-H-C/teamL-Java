f6ea2ff1ded652057b8fb288d16a3841cc6a218a
Masashi <masashi4406@gmail.com>
2025-06-18 14:13:55 +0900

    sales

--- src/main/java/com/example/beer/model/User.java
+++
@@ -1,5 +0,0 @@
-package com.example.beer.model;
-
-public class User {
-    
-}
--- src/main/java/com/example/beer/repository/BeerRepository.java
+++ src/main/java/com/example/beer/repository/BeerRepository.java
@@ -1,5 +1,10 @@
 package com.example.beer.repository;
 
-public class BeerRepository {
-    
+import com.example.beer.model.Beer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BeerRepository extends JpaRepository<Beer, Long> {
 }
+
