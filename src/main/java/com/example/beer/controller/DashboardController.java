d55be707fccc9da718340d90b249b0e463fb2b13
Masashi <masashi4406@gmail.com>
2025-06-18 13:22:07 +0900

    dashboard

--- src/main/java/com/example/beer/controller/DashboardController.java
+++ src/main/java/com/example/beer/controller/DashboardController.java
@@ -1,25 +1,16 @@
 package com.example.beer.controller;
 
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.security.core.Authentication;
-
 
 @Controller
 @RequestMapping("/dashboard")
 public class DashboardController {
 
     @GetMapping
-    public String showDashboard(Authentication authentication) {        //Authenticationは、Spring Securityが自動注入
-        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
-
-        if (userDetails.getAuthorities().stream()
-                .anyMatch(auth -> auth.getAuthority().equals("ROLE_ADMIN"))) {
-            return "dashboard/admin_dashboard";
-        } else {
-            return "dashboard/employee_dashboard"; 
-        }
+    public String showDashboard() {
+        // ログイン済みユーザーのロールに応じた出し分けは Thymeleaf 側で行う
+        return "dashboard/dashboard";  // templates/dashboard/dashboard.html を表示
     }
 }
